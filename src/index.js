4305c546438f4c893db339086d59e42ced1622bb
KolyaAlavackii <nikolayalavackii@gmail.com>
2016-11-15 11:46:49 +0200

    вот нормальный файл

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
-﻿function myFunction () {
-	a = 1;
-	b = 4;
-	x = a + b;
-	console.log(x);
-}
+﻿//var myArray = [2, 6, 20, 5, 50, "kdsjghs", 34, -25];
+  var min = function(array) {
+    var arrayLen = array.length,
+        minEl = array[0];
+    for (var i = 0; i < arrayLen; i++) {
+      if (minEl > array[i]) {
+        (minEl = array[i]);
+      }
+    }
+    return minEl;
+  }
+  //console.log(min(myArray));    
+
+
+
+ var max = function(array) {
+    var arrayLen = array.length,
+        maxEl = array[0];
+    for (var i = 0; i < arrayLen; i++) {
+      if (maxEl < array[i]) {
+        (maxEl = array[i]);
+      }
+    }
+    return maxEl;
+  }
+ // console.log(max(myArray)); 
+
+
+
+
+var sum = function(array) {
+    var sumEl = 0;
+    var arrayNumber = array.filter(function(number){
+    return number;
+    });
+
+    for (var i = 0; i < arrayNumber.length; i++) {
+      var arrayEl = typeof(arrayNumber[i]);
+      if(arrayEl != "number"){
+      arrayNumber[i] = 0;
+      };
+
+    sumEl += arrayNumber[i];
+    };
+
+    return sumEl;
+  };
+ // console.log(sum(myArray)); 
